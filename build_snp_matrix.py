9d7de6a79e7b45e62e07b1df1ac9759418eca118
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-04-14 17:04:40 +0100

    pseudocode

---
+++ build_snp_matrix.py
@@ -0,0 +1,43 @@
+import utils.utils
+
+def build_multi_fasta(plates_csv, bucket="s3-csu-003", keys=None):
+    """
+    # keys is a list of s3_uris - for bespoke requests
+    # if None download on plate-by-plate basis
+    if keys == None:
+        for plate_id in plate_ids:
+            s3_uris = [uri for uri in plates_csv]
+            append_multi_fasta(multi_fasta_path, s3_uris)
+    else:
+        append_multi_fasta(multi_fasta_path, keys)
+    """
+    pass
+
+def append_multi_fasta(multi_fasta_path, s3_uris):
+    """
+    1. dowloads consensus files from s3-csu-003 to a tmp directory
+    2. writes (appends) to multi fasta file
+    3. deletes tmp directory
+    """
+    pass
+
+def snps(output_path, multi_fasta_path):
+    """run snp-sites on consensus files, then runs snp-dists on the results"""
+
+    # run snp sites 
+    cmd = f'snp-sites {multi_fasta_path} -c -o {output_path}snpsites.fas'
+    utils.run(cmd, shell=True)
+
+    # run snp-dists
+    cmd = f'snp-dists {output_path}snpsites.fas > {output_path}snps.tab'
+    utils.run(cmd, shell=True)
+
+def main():
+    # parse plates.csv
+    # build multi-fasta
+    # run snp-sites
+    # run snp-dist
+    pass
+
+if __name__ == "__main__":
+    pass
---
+++ utils/utils.py
@@ -0,0 +1,31 @@
+import subprocess
+from io import StringIO
+import os
+import glob
+
+import pandas as pd
+
+def run(cmd, *args, **kwargs):
+    """ Run a command and assert that the process exits with a non-zero exit code.
+        See python's subprocess.run command for args/kwargs
+
+        Parameters:
+            cmd (list): List of strings defining the command, see (subprocess.run in python docs)
+            cwd (str): Set surr
+
+        Returns:
+            None: if capture_output == False (default)
+            process' stdout (str): if capture_output == True 
+    """
+    # TODO: store stdout to a file
+    ps = subprocess.run(cmd, *args, **kwargs)
+
+    returncode = ps.returncode
+    if returncode:
+        raise Exception("""*****
+            %s
+            cmd failed with exit code %i
+          *****""" % (cmd, returncode))
+
+    if "capture_output" in kwargs and kwargs["capture_output"]:
+        return ps.stdout.decode().strip('\n')
