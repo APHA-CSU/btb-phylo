f4cb67136c47c86970645174b81f794ecf2eb0af
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-04-22 18:27:34 +0100

    dev

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import subprocess
+import os
+import sys
+import errno
 
 import boto3
 import botocore
@@ -28,14 +31,13 @@ def run(cmd, *args, **kwargs):
     if "capture_output" in kwargs and kwargs["capture_output"]:
         return ps.stdout.decode().strip('\n')
 
-def s3_object_exists(bucket, key, endpoint_url):
+def s3_object_exists(bucket, key):
     """
         Returns true if the S3 key is in the S3 bucket. False otherwise
         Thanks: https://stackoverflow.com/questions/33842944/check-if-a-key-exists-in-a-bucket-in-s3-using-boto3
     """
-    
     key_exists = True
-    s3 = boto3.resource('s3', endpoint_url=endpoint_url)
+    s3 = boto3.resource('s3')
     try:
         s3.Object(bucket, key).load()
     except botocore.exceptions.ClientError as e:
@@ -45,5 +47,19 @@ def s3_object_exists(bucket, key, endpoint_url):
         else:
             # Something else has gone wrong.
             raise e
-
     return key_exists
+
+def s3_download(bucket, key, dest):
+    """
+        Downloads s3 object at the key-bucket pair (strings) to dest 
+        path (string)
+    """
+    #TODO: this is throwing an error which I currently don't understand
+    if s3_object_exists(bucket, key):
+        bucket = os.path.join(bucket, "")
+        key = os.path.join(key, "")
+        dest = os.path.join(dest, "")
+        run(["aws", "s3", "cp", "--recursive", "s3://" + bucket + key, dest])
+    else:
+        print("s3 key '{0}' does not exist in bucket '{1}'".format(key, bucket))
+        sys.exit(errno.EREMOTEIO)
