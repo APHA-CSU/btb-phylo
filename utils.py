ef1f846f4ed4478ec634deeaab425c8b13ace5f3
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-04-22 16:28:25 +0100

    s3_object_exists function

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import subprocess
 
+import boto3
+import botocore
+
 def run(cmd, *args, **kwargs):
     """ Run a command and assert that the process exits with a non-zero exit code.
         See python's subprocess.run command for args/kwargs
@@ -24,3 +27,23 @@ def run(cmd, *args, **kwargs):
 
     if "capture_output" in kwargs and kwargs["capture_output"]:
         return ps.stdout.decode().strip('\n')
+
+def s3_object_exists(bucket, key, endpoint_url):
+    """
+        Returns true if the S3 key is in the S3 bucket. False otherwise
+        Thanks: https://stackoverflow.com/questions/33842944/check-if-a-key-exists-in-a-bucket-in-s3-using-boto3
+    """
+    
+    key_exists = True
+    s3 = boto3.resource('s3', endpoint_url=endpoint_url)
+    try:
+        s3.Object(bucket, key).load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            # The object does not exist.
+            key_exists = False
+        else:
+            # Something else has gone wrong.
+            raise e
+
+    return key_exists
