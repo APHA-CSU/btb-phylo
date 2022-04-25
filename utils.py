b25ba3a802795fe51350debf84df64ecb1d3703b
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-04-25 11:56:27 +0100

    check s3 folder exists

--- utils.py
+++ utils.py
@@ -6,6 +6,18 @@ import errno
 import boto3
 import botocore
 
+def s3_folder_exists(bucket, path):
+    """
+        Returns true if the folder is in the S3 bucket. False otherwise
+    """
+    exists = False
+    client = boto3.client('s3')
+    path = os.path.join(path, "")
+    response = client.list_objects(Bucket=bucket, Prefix=path, MaxKeys=1)
+    if 'Contents' in response:
+        exists = True
+    return exists
+
 def run(cmd, *args, **kwargs):
     """ Run a command and assert that the process exits with a non-zero exit code.
         See python's subprocess.run command for args/kwargs
@@ -31,35 +43,16 @@ def run(cmd, *args, **kwargs):
     if "capture_output" in kwargs and kwargs["capture_output"]:
         return ps.stdout.decode().strip('\n')
 
-def s3_object_exists(bucket, key):
+def s3_download_folder(bucket, key, dest):
     """
-        Returns true if the S3 key is in the S3 bucket. False otherwise
-        Thanks: https://stackoverflow.com/questions/33842944/check-if-a-key-exists-in-a-bucket-in-s3-using-boto3
-    """
-    key_exists = True
-    s3 = boto3.resource('s3')
-    try:
-        s3.Object(bucket, key).load()
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            # The object does not exist.
-            key_exists = False
-        else:
-            # Something else has gone wrong.
-            raise e
-    return key_exists
-
-def s3_download(bucket, key, dest):
-    """
-        Downloads s3 object at the key-bucket pair (strings) to dest 
+        Downloads s3 folder at the key-bucket pair (strings) to dest 
         path (string)
     """
-    #TODO: this is throwing an error which I currently don't understand
-    if s3_object_exists(bucket, key):
-        bucket = os.path.join(bucket, "")
+    if s3_folder_exists(bucket, key):
         key = os.path.join(key, "")
+        bucket = os.path.join(bucket, "")
         dest = os.path.join(dest, "")
         run(["aws", "s3", "cp", "--recursive", "s3://" + bucket + key, dest])
     else:
-        print("s3 key '{0}' does not exist in bucket '{1}'".format(key, bucket))
-        sys.exit(errno.EREMOTEIO)
+        print("here")
+        raise Exception("Path '{0}' does not exist in bucket '{1}'".format(key, bucket))
