9b2d0348df100bd9c4c48a305e136d75c56ae059
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-07-07 14:44:40 +0100

    make unit tests pass

--- unit_tests.py
+++ unit_tests.py
@@ -11,15 +11,42 @@ import update_summary
 
 class TestBtbPhylo(unittest.TestCase):
     def test_remove_duplicates(self):
-        # define dataframe for input
+        # test 2 kwargs
         test_df = pd.DataFrame({"Submission":pd.Series(["1", "2", "2", "2", "1", "3"], dtype="object"),
                                 "pcMapped":pd.Series([0.1, 0.2, 0.3, 0.4, 0.1, 0.6], dtype=float),
                                 "Ncount":pd.Series([1, 2, 3, 4, 5, 6], dtype=float)})
-        print("a", id(test_df))
-        # test expected input
-        nptesting.assert_array_equal(btb_phylo.remove_duplicates(test_df).values,
+        nptesting.assert_array_equal(btb_phylo.remove_duplicates(test_df, pcMapped="max", Ncount="min").values,
                                      pd.DataFrame({"Submission":["1", "2", "3"], 
                                                    "pcMapped":[0.1, 0.4, 0.6], "Ncount":[1, 4, 6]}).values)
+        # test 3 kwargs
+        test_df = pd.DataFrame({"Submission":pd.Series(["1", "2", "2", "2", "1", "3", "1"], dtype="object"),
+                                "pcMapped":pd.Series([0.1, 0.2, 0.3, 0.4, 0.1, 0.6, 0.1], dtype=float),
+                                "Ncount":pd.Series([1, 2, 3, 4, 5, 6, 1], dtype=float),
+                                "foo":pd.Series([10, 20, 30, 40, 50, 60, 70], dtype=float)})
+        nptesting.assert_array_equal(btb_phylo.remove_duplicates(test_df, pcMapped="max", Ncount="min", foo="max").values,
+                                     pd.DataFrame({"Submission":["2", "3", "1"], 
+                                                   "pcMapped":[0.4, 0.6, 0.1], "Ncount":[4, 6, 1], "foo":[40, 60, 70]}).values)
+        # test defaulting to first sample
+        test_df = pd.DataFrame({"Submission":pd.Series(["1", "2", "2", "2", "1", "3", "1"], dtype="object"),
+                                "pcMapped":pd.Series([0.1, 0.2, 0.3, 0.4, 0.1, 0.6, 0.1], dtype=float),
+                                "Ncount":pd.Series([1, 2, 3, 4, 5, 6, 1], dtype=float),
+                                "foo":pd.Series([10, 20, 30, 40, 50, 60, 10], dtype=float)})
+        nptesting.assert_array_equal(btb_phylo.remove_duplicates(test_df, pcMapped="max", Ncount="min", foo="max").values,
+                                     pd.DataFrame({"Submission":["1", "2", "3"], 
+                                                   "pcMapped":[0.1, 0.4, 0.6], "Ncount":[1, 4, 6], "foo":[10, 40, 60]}).values)
+        # test exceptions
+        with self.assertRaises(btb_phylo.InvalidDtype):
+            btb_phylo.remove_duplicates(pd.DataFrame({"Submission":pd.Series(["1"], dtype="object"),
+                                        "foo":pd.Series(["a"], dtype="object")}), foo="max")
+        with self.assertRaises(ValueError):
+            btb_phylo.remove_duplicates(pd.DataFrame({"Submission":pd.Series(["1"], dtype="object"),
+                                        "foo":pd.Series([1], dtype=float)}), bar="max")
+        with self.assertRaises(ValueError):
+            btb_phylo.remove_duplicates(pd.DataFrame({"Submission":pd.Series(["1"], dtype="object"),
+                                        "foo":pd.Series([1], dtype=float)}), foo="bar")
+        with self.assertRaises(TypeError):
+            btb_phylo.remove_duplicates(pd.DataFrame({"Submission":pd.Series(["1"], dtype="object"),
+                                        "foo":pd.Series([1], dtype=float)}))
 
 #    def test_get_indexes_to_remove(self):
 #        # define dataframe for input
