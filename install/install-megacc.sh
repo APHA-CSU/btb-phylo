2400ccad307fa20be7a6711df1b1bb25c772ed51
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-07-11 14:24:36 +0100

    bug fix

--- install/install-megacc.sh
+++ install/install-megacc.sh
@@ -3,5 +3,5 @@
 set -e
 
 wget https://www.megasoftware.net/do_force_download/megax-cc_10.2.6-1_amd64.deb
-apt-get install -y ./mega-cc_11.0.13-1_amd64.deb
+apt-get install -y ./mega-cc_10.2.6-1_amd64.deb
 rm mega-cc_11.0.13-1_amd64.deb
