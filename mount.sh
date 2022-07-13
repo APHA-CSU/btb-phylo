8e7809ad0a5c167ddb7a33b43456433040d239e3
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-07-13 09:44:11 +0100

    'host user'

--- mount.sh
+++ mount.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 set -e
 
+HOSTUSER=$0
+
 REMOTE_SHARE=//fsx-ranch-017.int.sce.network/share/fsx-017
 MOUNT_POINT=/mnt/fsx-017
 
@@ -10,6 +12,6 @@ if grep -qs "$REMOTE_SHARE" /proc/mounts; then
 	    printf " - Already mounted.  See details:\n $(grep $REMOTE_SHARE /proc/mounts)\n - Exiting.\n"
     else
 		mkdir /mnt/fsx-017
-	        mount -t cifs -o vers=2.1,sec=ntlmsspi,user=$USER,uid=$USER $REMOTE_SHARE $MOUNT_POINT \
-			    && echo " - Successfully mounted at: $MOUNT_POINT"
+		mount -t cifs -o vers=2.1,sec=ntlmsspi,user=$HOSTUSER,uid=$HOSTUSER $REMOTE_SHARE $MOUNT_POINT \
+			&& echo " - Successfully mounted at: $MOUNT_POINT"
 fi
