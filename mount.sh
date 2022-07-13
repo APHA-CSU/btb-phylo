9e9e790c997cf4e947c0124edf38e9f61ce62393
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-07-13 10:07:51 +0100

    remove user

--- mount.sh
+++ mount.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
 set -e
 
-HOSTUSER=$0
-
 REMOTE_SHARE=//fsx-ranch-017.int.sce.network/share/fsx-017
 MOUNT_POINT=/mnt/fsx-017
 
@@ -12,6 +10,6 @@ if grep -qs "$REMOTE_SHARE" /proc/mounts; then
 	    printf " - Already mounted.  See details:\n $(grep $REMOTE_SHARE /proc/mounts)\n - Exiting.\n"
     else
 		mkdir /mnt/fsx-017
-		mount -t cifs -o vers=2.1,sec=ntlmsspi,user=$HOSTUSER,uid=$HOSTUSER $REMOTE_SHARE $MOUNT_POINT \
+		mount -t cifs -o vers=2.1,sec=ntlmsspi $REMOTE_SHARE $MOUNT_POINT \
 			&& echo " - Successfully mounted at: $MOUNT_POINT"
 fi
