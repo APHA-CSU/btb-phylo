6a53a43a63072d1183cc75924ee960ae40c62ce7
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-07-14 10:23:25 +0100

    bind mount consensus path

--- install/install.bash
+++ install/install.bash
@@ -15,7 +15,6 @@ apt-get -y update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
     python3 \
     python3-pip \
     wget \
-    cifs-utils \
     awscli
 
 pip3 install -r requirements.txt
