f72b8a7ec625f0e72e2543c6b33ac61de8a9aa23
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-07-26 15:59:06 +0100

    delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import boto3
-import botocore
-import btbphylo.utils as utils
-
-boto3.set_stream_logger('')
-utils.s3_download_file("s3-csu-003", "v3-2/Results_10049_27Jun22/consensus/AF-12-01212-17_consensus.fas", "/home/nickpestell/tmp/test.fas")
