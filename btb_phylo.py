a6de9ce58557bbbe44e594bd7b5e0d632eaf1677
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-11-14 17:02:50 +0000

    testing feature of sample filter on ViewBovine

--- btb_phylo.py
+++ btb_phylo.py
@@ -702,7 +702,7 @@ def parse_args():
         nargs=2, help="optional filter")
     subparser.set_defaults(func=full_pipeline)
 
-    # view bovine
+    # viewbovine
     subparser = subparsers.add_parser('ViewBovine', help="runs phylogeny with \
         default settings for ViewBovine")
     subparser.add_argument("results_path", help="path to results directory")
@@ -720,6 +720,9 @@ def parse_args():
         type=float, nargs=2, help="optional filter", default=(90, 100))
     subparser.add_argument("--flag", "-f", dest="flag", nargs="+", \
         help="optional filter", default=["BritishbTB"])
+    # TODO: remove this argument as it's just here for testing purposes
+    subparser.add_argument("--sample_name", "-s", dest="Sample", nargs="+", \
+        help="optional filter")
     subparser.set_defaults(func=view_bovine)
 
     # pasre args
